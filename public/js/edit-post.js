c33a4d38d3d14eb65f9984c9d329e5f1ba195d29
SDeGraff <114950127+SDeGraff@users.noreply.github.com>
2023-02-09 20:33:14 -0600

    edit post js

---
+++ public/js/edit-post.js
@@ -0,0 +1,25 @@
+const postId = document.querySelector('input[name="post-id"]').value;
+
+const editFormHandler = async function(event) {
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value;
+  const body = document.querySelector('textarea[name="post-body"]').value;
+
+  await fetch(`/api/post/${postId}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      title,
+      body
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  document.location.replace('/dashboard');
+};
+
+document
+  .querySelector('#edit-post-form')
+  .addEventListener('submit', editFormHandler);
