408bf52f82be7e8052854e830f3417fd68718c38
Lance_Lake <temp@MostDeadlyGame.com>
2023-02-09 21:55:25 -0500

    Hashes fixed!

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,36 +3,23 @@ const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
-
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render('homepage', {
-      users,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+	res.render('homepage');
 });
 
 router.get('/login', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
-
-  res.render('login');
+	res.render('login');
 });
 
 router.get('/signup', (req, res) => {
-
-  res.render('signup');
+	res.render('signup');
 });
 
+router.get('/logout', (req, res) => {
+	res.render('homepage');
+});
+
+router.get('/post', (req, res) => {
+	res.render('post');
+});
 
 module.exports = router;
