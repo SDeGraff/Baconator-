f34cda2e108d4185bdc6c9f486f93c1d155ab17b
Mike B <115735557+mbartnett@users.noreply.github.com>
2023-02-14 15:43:23 -0600

    conflicts

---
+++ models/Post.js
@@ -0,0 +1,78 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {
+	//	checkPassword(loginPw) {
+	//	return bcrypt.compareSync(loginPw, this.password);
+	//	}
+}
+
+Post.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		title: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		message: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+		date_created: {
+			type: DataTypes.DATE,
+			allowNull: false,
+			defaultValue: DataTypes.NOW
+		}
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'post',
+	}
+);
+
+
+module.exports = Post;
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {
+	//	checkPassword(loginPw) {
+	//	return bcrypt.compareSync(loginPw, this.password);
+	//	}
+}
+
+Post.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		title: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		message: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'post',
+	}
+);
+
+module.exports = Post;
