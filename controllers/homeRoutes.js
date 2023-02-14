072a2874703d0e39c86cc1398b1043aba4306e8f
Lance_Lake <temp@MostDeadlyGame.com>
2023-02-13 22:34:10 -0500

    Completed routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
 	res.render('homepage');
 });
 
