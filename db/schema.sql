ff147defb358f982b7055912305fbd980bba35c5
Lance_Lake <temp@MostDeadlyGame.com>
2023-02-13 20:41:53 -0500

    Updated Schema File

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,26 @@
 DROP DATABASE IF EXISTS user_db;
 CREATE DATABASE user_db;
+
+-- Posts Table
+CREATE TABLE posts (
+	ID INT(10) NOT NULL AUTO_INCREMENT,
+	Title VARCHAR(100) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	Message TEXT NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	UNIQUE INDEX ID (ID) USING BTREE
+)
+COLLATE='utf8mb4_0900_ai_ci'
+ENGINE=InnoDB
+;
+
+-- User Table
+CREATE TABLE `user` (
+	`id` INT(10) NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`email` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`password` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	PRIMARY KEY (`id`) USING BTREE,
+	UNIQUE INDEX `email` (`email`) USING BTREE
+)
+COLLATE='utf8mb4_0900_ai_ci'
+ENGINE=InnoDB
+;
