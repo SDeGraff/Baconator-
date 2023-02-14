55d2ac34f20fc486fd144ebb391fa5d4b2fb557f
Mike B <115735557+mbartnett@users.noreply.github.com>
2023-02-13 21:49:31 -0600

    trying to seed posts data to db

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,8 @@
 const sequelize = require('../config/connection');
-const { User, Post } = require('../models');
+const { User, Posts } = require('../models');
 
 const userData = require('./userData.json');
-const postData = require('./postData.json');
+const postsData = require('./postsData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
