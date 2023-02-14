549a08edfb15d279fee11eb78e56e20457ffe5b3
Lance_Lake <temp@MostDeadlyGame.com>
2023-02-13 19:25:06 -0500

    Changing API setup to not use User

---
+++ models/Posts.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Posts extends Model {
+//	checkPassword(loginPw) {
+//	return bcrypt.compareSync(loginPw, this.password);
+//	}
+}
+
+Posts.init(
+	{
+	id: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		primaryKey: true,
+		autoIncrement: true,
+	},
+	title: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	body: {
+		type: DataTypes.TEXT,
+		allowNull: false,
+	},
+  },
+  {
+	sequelize,
+	timestamps: false,
+	freezeTableName: true,
+	underscored: true,
+	modelName: 'posts',
+  }
+);
+
+module.exports = Posts;
