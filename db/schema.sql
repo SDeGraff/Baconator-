ce719a00031657a36419cb8f1dfdf1ef4aba9e77
SDeGraff <114950127+SDeGraff@users.noreply.github.com>
2023-02-21 14:48:33 -0600

    testing schema.db

--- db/schema.sql
+++ db/schema.sql
@@ -3,17 +3,17 @@ CREATE DATABASE user_db;
 USE user_db;
 
 -- Posts Table
-CREATE TABLE posts (
-	`ID` INT(10) NOT NULL AUTO_INCREMENT,
-	`Title` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
-	`Message` TEXT NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
-	`createdat` datetime DEFAULT NULL COMMENT 'created time',
-	`updatedat` datetime DEFAULT NULL COMMENT 'updated time',
-	UNIQUE INDEX ID (ID) USING BTREE
-)
-COLLATE='utf8mb4_0900_ai_ci'
-ENGINE=InnoDB
-;
+-- CREATE TABLE posts (
+-- 	`ID` INT(10) NOT NULL AUTO_INCREMENT,
+-- 	`Title` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+-- 	`Message` TEXT NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+-- 	`createdat` datetime DEFAULT NULL COMMENT 'created time',
+-- 	`updatedat` datetime DEFAULT NULL COMMENT 'updated time',
+-- 	UNIQUE INDEX ID (ID) USING BTREE
+-- )
+-- COLLATE='utf8mb4_0900_ai_ci'
+-- ENGINE=InnoDB
+-- ;
 
 -- User Table
 --CREATE TABLE `user` (
